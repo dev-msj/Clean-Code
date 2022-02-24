02fe27fdb99432f78a64515c9485d450775f2360
dev-msj <msj944@naver.com>
2022-02-25 04:56:04 +0900

    2장 마크다운/예제코드 수정

--- seungjun-moon/code/2. 의미 있는 이름/ex5.py
+++ seungjun-moon/code/2. 의미 있는 이름/ex5.py
@@ -1,17 +1,21 @@
 """
 인코딩을 피하라
+
+Part처럼 변수명에 범위와 변수 타입까지 있으면 해당 정보에 대한 인코딩(분석)을 해야하고 이름이 쓸데없이 길어진다.
+
+PartRemoveEncoding에서는 __를 붙여 변수를 private으로 만들고, 타입을 명시하여 이름에서 필요없는 정보를 없앴다.
 """
 
 
 class Part:
-    __m_dsc: str
+    private_str_my_name = None
 
     def set_name(self, name):
-        self.__m_dsc = name
+        self.private_str_my_name = name
 
 
-class PartRemovePrefix:
-    __name: str
+class PartRemoveEncoding:
+    __my_name: str = None
 
     def set_name(self, name):
-        self.__name = name
+        self.__my_name = name
